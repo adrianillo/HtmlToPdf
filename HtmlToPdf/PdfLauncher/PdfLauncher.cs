789c4b52c2ae5211db2de4f289d9670bccdfce36
adrianillo <elcorreillodeadrianillo@gmail.com>
2013-06-19 23:10:39 +0200

    v 1.0

--- HtmlToPdf/PdfLauncher/PdfLauncher.cs
+++ HtmlToPdf/PdfLauncher/PdfLauncher.cs
@@ -33,8 +33,7 @@ namespace Example1
             sb.WriteLine("</style>");
             sb.WriteLine("</head>");
             sb.WriteLine("<body>");
-            sb.WriteLine("<code>");
-            sb.WriteLine("<%@ Page Language='C#' AutoEventWireup='true' CodeBehind='Default.aspx.cs' Inherits='WebApplication1._Default' %>");
+            sb.WriteLine("<code>");       
             sb.WriteLine("</code>");
             sb.WriteLine("<p class='clas1'>Example 1</p>");
             sb.WriteLine("<table class='punteado'><tr><td> <table style='background-color:yellow;border-width: 1px;width:250px;height:200px;'>");
@@ -43,18 +42,17 @@ namespace Example1
             sb.WriteLine("</td><td>1º <b>row</b> 2º col</td></tr><tr><td style='text-align:right'>2º <h2>row</h2>");
             sb.WriteLine("<span style='color:blue;'>1º</span> col</td><td>2º row 2 col</td></tr></table>");
             sb.WriteLine("<h1 id='search-box'>h1 title</h1>");
+            sb.WriteLine("<br/>");
             sb.WriteLine("<h2> h2 title</h2>");
             sb.WriteLine("<br/>");
-            sb.WriteLine("<h3> h3 title</h3>");
-            sb.WriteLine("<h3> h3(2) title</h3>");
+            sb.WriteLine("<h3> h3 title</h3>");                      
+            sb.WriteLine("<br/>");
             sb.WriteLine("<h4> h4 title</h4>");
-            sb.WriteLine("<h4> h4(2) title</h4>");
+            sb.WriteLine("<br/>");
             sb.WriteLine("<h5> h5 title</h5>");
-            sb.WriteLine("<h5> h5(2) title</h5>");
+            sb.WriteLine("<br/>");
             sb.WriteLine("<h6> h6 title</h6>");
-            sb.WriteLine("<h6> h6(2) title</h6>");
-            sb.WriteLine("<h6> h6(3) title</h6>");
-            sb.WriteLine("<h2> h2(2) title</h2>");
+            sb.WriteLine("<br/>");        
             sb.WriteLine("<h3> h3<div class='clase1'>title<div style='color:#4EE636;font-size:12px;'>in  div</div></div> </h3>");
             sb.WriteLine("<h4> h4 title</h4>");
             sb.WriteLine("<h5> h5 title</h5>");
@@ -67,10 +65,10 @@ namespace Example1
             sb.WriteLine("<p align=\"right\">right aligned</p>");
             sb.WriteLine("<p style='color:blue;' align=\"center\">centered</p>");
             sb.WriteLine("<p style='text-align:right;width:40px;'>paragraph with <b>bold (b)</b> elements</p>");
-            sb.WriteLine("<p class='clas1'>paragraph <b>with</b> emphasized (em) elements</p>");       
-            sb.WriteLine("<p class='clas2'>paragraph with <tt>typewriter (tt)</tt> elements</p>");
+            sb.WriteLine("<p class='clas1'>paragraph <b>with</b> elements</p>");       
+            sb.WriteLine("<p class='clas2'>paragraph example</p>");
             sb.WriteLine("<p id='prf' >paragraph with <code>code (code)</code> elements</p>");
-            sb.WriteLine("<p class='alert-error'>paragraph with <span class=\"em\">span elements (class = em)</span></p>");
+            sb.WriteLine("<p class='alert-error'>paragraph with <span class=\"em\">span elements )</span></p>");
             sb.WriteLine("<p class='classone punteado'>paragraph <p> par 2</p> par 1-2 </p>");
             sb.WriteLine("<ul style='list-style-type: disc;'><li>Unordered 1</li><li>Unordered 2</li></ul>");
             sb.WriteLine("<ol class='classone'><li>Ordered 1</li><li>Ordered 2</li></ol>");            
@@ -105,31 +103,31 @@ namespace Example1
             HtmlToPdf.HtmlToPdf docHtml = new HtmlToPdf.HtmlToPdf();
             try
             {
-                HtmlSection sec1 = docHtml.AddSection("Section 1");      
+                //add section 1
+                HtmlSection sec1 = docHtml.AddSection("Section 1");    
+                //add styles
                 List<HtmlClass> ListCss = new List<HtmlClass>();
                 HtmlClass c1 = new CssParser().Parser(stl.ToString());
                 c1.IdHtmlClass = "sss";
                 ListCss.Add(c1);
+                //add html
                 HtmlDoc html = docHtml.AddHtml(sb.ToString(), ListCss, sec1);
 
+                //add section 2
                 HtmlSection sec2 = docHtml.AddSection("Section 2");
-                docHtml.AddHtml(sb.ToString(), sec2);
-
                 string htmldoc = "<html><head><body><p style='color:blue;font-size:20px;' >";
                 htmldoc += "Hola Mundo</p></body></head></html>";
-
                 htmldoc = "<html xmlns='http://www.w3.org/1999/xhtml'><head><title /></head><body><div style='clear: both'><p style='font: 13px/20.79px sans-serif, Arial, Verdana, Trebuchet MS; color: rgb(51, 51, 51); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; font-size-adjust: none; font-stretch: normal; -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px;'><span style='color: rgb(0, 0, 0);'><span style='font-size: 18px;'><span style='font-family: verdana, geneva, sans-serif;'>Requirimientos</span></span></span></p>";
-
                 htmldoc += "<p style='font: 13px/20.79px sans-serif, Arial, Verdana, Trebuchet MS; color: rgb(51, 51, 51); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; white-space: normal; font-size-adjust: none; font-stretch: normal; -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px;'> </p>";
-
                 htmldoc += "<ul style='font: 14px/22px Open Sans, Arial Helvetica, sans-serif; margin: 0px; padding: 0px 0px 0px 1.5em; border: 0px currentColor; color: rgb(70, 70, 70); text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; vertical-align: baseline; white-space: normal; font-size-adjust: none; font-stretch: normal; -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px;'>";
                 htmldoc += "<li style='margin: 0px 0px 0.5em; padding: 0px; border: 0px currentColor; line-height: inherit; font-family: inherit; font-size: inherit; font-style: inherit; font-variant: inherit; vertical-align: baseline;'><span style='color: rgb(0, 0, 0);'><span style='font-size: 14px;'><span style='font-family: verdana, geneva, sans-serif;'>jQuery 1.4.x o superior</span></span></span></li>";
                 htmldoc += "	<li style='margin: 0px 0px 0.5em; padding: 0px; border: 0px currentColor; line-height: inherit; font-family: inherit; font-size: inherit; font-style: inherit; font-variant: inherit; vertical-align: baseline;'><span style='color: rgb(0, 0, 0);'><span style='font-size: 14px;'><span style='font-family: verdana, geneva, sans-serif;'>Un servidor capaz de analizar PHP, ASP.Net, JSP, o un lenguaje similar del lado del servidor</span></span></span></li>";
                 htmldoc += "</ul>";
                 htmldoc += "</div></body></html>";
-
+                //add html
                 docHtml.AddHtml(sb.ToString(), sec2);
 
+                //generate pdf
                 docHtml.Generate("e:\\test.pdf");
               
             }
